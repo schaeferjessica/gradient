1c33f7c27651dba2984c94865626108efcdc9c99
jschaefer <jschaefer@3pc.de>
2021-01-20 14:45:41 +0100

    feat: check for h2 contrast with chroma luminace

--- app.js
+++ app.js
@@ -20,7 +20,18 @@ function randomColors(){
     // add the color to the background 
     div.style.backgroundColor = randomColor;
     hexH2.innerText = randomColor;
+    // check for contrast
+    checkTextContrast(randomColor, hexH2);
   });
+}
+
+function checkTextContrast(color,text) {
+  const luminance = chroma(color).luminance();
+  if(luminance > 0.5){
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
 }
 
 randomColors();
